8d2bc7fae8c477a52afed36002a5fa80172c20da
jquense <monastic.panic@gmail.com>
2015-06-18 14:25:28 -0400

    Load custom eslint rules, including a spread aware shorthand rule
    Fixes that dang false eslint warning about using spread properties in an
    object.
    
    Prior art on this: https://github.com/facebook/react/pull/4034

---
+++ tools/eslint-rules/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'lodash-import': require('./lodash-import')
+  }
+};
---
+++ tools/eslint-rules/lodash-import.js
@@ -0,0 +1,22 @@
+/*eslint-disable */
+/**
+ * @fileoverview Rule to enforce entire lodash library is not imported
+ * @author Matt Smith
+ * @copyright 2015 Matt Smith. All rights reserved.
+ */
+
+'use strict';
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = function(context) {
+  return {
+    ImportDeclaration: function(node) {
+      if (node.source.value === 'lodash') {
+        context.report(node.source, 'Importing the entire lodash library is not permitted, please import the specific functions you need');
+      }
+    }
+  };
+};
